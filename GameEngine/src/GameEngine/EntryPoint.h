bcbc21b0c20bff1222483c55fd6f47fa2ba9bfb5
Mário Agostinho <mariojgagostinho@gmail.com>
2023-07-18 01:48:48 +0100

    Added logging with spdlog

--- GameEngine/src/GameEngine/EntryPoint.h
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -6,6 +6,10 @@ extern GameEngine::Application* GameEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	GameEngine::Log::Init();
+	GE_CORE_WARN("Initialized Log!");
+	GE_INFO("Hello!");
+
 	auto app = GameEngine::CreateApplication();
 	app->Run();
 	delete app;
