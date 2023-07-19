430d5edea3a00fe2c263158c6ca035e547673eaa
Mário Agostinho <mariojgagostinho@gmail.com>
2023-07-19 17:55:03 +0100

    Added precompiled headers

--- GameEngine/src/GameEngine/Events/KeyEvent.h
+++ GameEngine/src/GameEngine/Events/KeyEvent.h
@@ -2,8 +2,6 @@
 
 #include "Event.h"
 
-#include <sstream>
-
 namespace GAMEENGINE
 {
 	class GE_API KeyEvent : public Event
--- GameEngine/src/GameEngine/Events/MouseEvent.h
+++ GameEngine/src/GameEngine/Events/MouseEvent.h
@@ -2,8 +2,6 @@
 
 #include "Event.h"
 
-#include <sstream>
-
 namespace GAMEENGINE
 {
 	class GE_API MouseMovedEvent : public Event
